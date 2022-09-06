006e87f402bcaf11e3924dee1af86734568749dd
v1k65 <46851576+v1k65@users.noreply.github.com>
2022-09-06 15:40:37 -0400

    Initial Commit

---
+++ MetalShaderShowcase macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  MetalShaderShowcase macOS
+//
+//  Created by v1k65m677 on 9/6/22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+	func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+		return true
+	}
+
+
+}
+
